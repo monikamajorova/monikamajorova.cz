3ff5a9e8fc5f6410d50606c35e899f678e65203f
Monika Majorová <monika@monikamajorova.cz>
2020-04-21 20:11:02 +0200

    Try to fix values sent to Toplist.

--- src/components/toplist.js
+++ src/components/toplist.js
@@ -1,16 +1,26 @@
 import React from "react"
 import styles from "./toplist.module.css"
+import { Helmet } from "react-helmet"
 
-const Toplist = () => (<img src="https://toplist.cz/dot.asp?id=1799006
-        &http={escape(document.referrer)}
-        &t={escape(document.title)}
-        &wi={escape(window.screen.width)}
-        &he={escape(window.screen.height)}
-        &cd={escape(window.screen.colorDepth)}"
-     alt="TOPlist"
-     width="1"
-     height="1"
-     className={styles.toplist} />
+const Toplist = () => (
+    <>
+        <img src=""
+            id="toplistImg"
+            alt="TOPlist"
+            width="1"
+            height="1"
+            className={styles.toplist} />
+        <Helmet>
+            <script>
+                const toplistImgSrc = "https://toplist.cz/dot.asp?id=1799006&http="+escape(document.referrer)
+    +"&t="+escape(document.title)
+    +"&wi="+escape(window.screen.width)
+    +"&he="+escape(window.screen.height)
+    +"&cd="+escape(window.screen.colorDepth);
+                document.getElementById("toplistImg").src = toplistImgSrc;
+            </script>
+        </Helmet>
+    </>
 )
 
 export default Toplist
